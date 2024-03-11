93a31244dac5fb6bcffbda0d2c68ef69541fe82f
aeong <aevalog@gmail.com>
2024-03-11 22:09:45 +0900

    upload demo project

---
+++ src/main/java/com/example/springaj/SpringajApplication.java
@@ -0,0 +1,13 @@
+package com.example.springaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringajApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springaj/SpringajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
